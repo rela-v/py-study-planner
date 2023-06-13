4018d500f957d936685efa3cdb194d7591c074f2
Rela Vergis <rela.mngrbot@gmail.com>
2023-06-13 00:58:32 -0400

    completed edit_exam function

--- main.py
+++ main.py
@@ -11,6 +11,16 @@ def random_number_gen(n):
     range_end = (10**n)-1
     return random.randint(range_start, range_end)
 
+def database_id_search(dictionary, exam_name):
+    """traverse through database values to get matching exam_name and return associated id"""
+    for key, value in dictionary.items(): # for each key, value pair
+        if isinstance(value, dict): #otherwise, try searching for a dictionary in the keys
+            for val in value.values():
+                if val==exam_name:
+                    return key
+    print("key not found")
+    return None
+
 def print_user_options():
     """presents options for user in menu"""
     print("('a' and hit enter to add an exam that you are studying for)")
@@ -19,19 +29,28 @@ def print_user_options():
     print("('d' and hit enter to delete an exam that you are currently studying for)")
     print("('q' and hit enter to quit py-study-planner)")
 
-def add_exam():
-    """routine for adding user input on exam data to json"""
-    exam_name = input("What is the name of the exam you'd like to add?\n")
-    exam_date = input("What date is your exam? (use mm/dd/yyyy format)\n")
-    resources = input("List any resources that you have available, separated by a space (e.g. 'textbook website flashcards' inputs 3 separate resources).\n")
+def get_resource_data():
+    """get data for resources according to user input"""
+    resources = input("List resources, separated by a space " \
+                      "(e.g. 'textbook website flashcards' inputs 3 separate resources).\n")
     resources = resources.split(' ')
     resources_times = []
     for i, resource in enumerate(resources):
         if i>len(resources):
             break
-        resource_time = input(f"How much time would you like to dedicate to the '{resource}' resource? Enter how many total minutes you want to spend on this resource from now until the exam.\n")
+        resource_time = input("How much time would you like to dedicate " \
+                              f"to the '{resource}' resource? Enter how many total " \
+                                  "minutes you want to spend on this resource from now " \
+                                    "until the exam.\n")
         resources_times.append(resource_time)
     resource_data = list(zip(resources, resources_times))
+    return resource_data
+
+def add_exam():
+    """routine for adding user input on exam data to json"""
+    exam_name = input("What is the name of the exam you'd like to add?\n")
+    exam_date = input("What date is your exam? (use mm/dd/yyyy format)\n")
+    resource_data = get_resource_data()
     exam_info = {'exam_name': exam_name, 'exam_date': exam_date, 'resource_data': resource_data}
     if os.path.exists('exam_data.json'): #if the json database exists then update
     # search ids in order to produce a new, unique id
@@ -71,6 +90,31 @@ def add_exam():
 
     return exam_data
 
+def edit_exam(exam_name):
+    """routine for editing user input on exam data to json"""
+    with open('exam_data.json',"r", encoding='utf-8') as exam_data_file:
+        loaded = json.load(exam_data_file)
+    exam_id = database_id_search(loaded, exam_name)
+    if exam_id:
+        change_flags = input(f"Type the things you wanted to change about your {exam_name} " \
+                            "exam separated by a space: type...\n"\
+                            "('n' to edit the name of the exam)\n"\
+                            "('d' to edit the date of the exam)\n"\
+                            "('r' to edit the resources of the exam)\n")
+        change_flags = change_flags.split(' ')
+        if "n" in change_flags:
+            loaded[exam_id]['exam_name'] = input(f"Please input the new name for your {exam_name} "\
+                                                "exam\n")
+        if "d" in change_flags:
+            loaded[exam_id]['exam_date'] = input(f"Please input the new date for your {exam_name} "\
+                                                "exam in 'mm/dd/yyyy' format\n")
+        if "r" in change_flags:
+            loaded[exam_id]['resource_data'] = get_resource_data()
+        with open('exam_data.json',"w", encoding='utf-8') as exam_data_file:
+            json.dump(loaded, exam_data_file)
+        return True
+    return False
+
 def main():
     """run main function while constant "run" is True, allows user to navigate main menu or quit"""
     print("Welcome to the py-study-planner program.")
@@ -84,12 +128,24 @@ def main():
             names = []
             for d in exam_data.values():
                 names.append(d['exam_name'])
-            print(f"{names[-1]} was added to the database (you can find the database under 'exam_data.json')! What would you like to do next?")
+            print(f"{names[-1]} was added to the database "\
+                  "(you can find the database under 'exam_data.json')! "\
+                   "What would you like to do next?")
             print_user_options()
         elif usr_input_mm=="l":   # list all exams in json database
             print(usr_input_mm)
         elif usr_input_mm=="e":   # edit exam in json json database
-            print(usr_input_mm)
+            exam_to_edit = input("What exam would you like to edit?\n")
+            exam_found = edit_exam(exam_to_edit)
+            if exam_found:
+                print(f"{exam_to_edit} was changed in the database! "\
+                    "(you can find the database under 'exam_data.json')! "\
+                    "What would you like to do next?")
+                print_user_options()
+            else:
+                print(f"{exam_to_edit} was not found. "\
+                    "What would you like to do next?")
+                print_user_options()
         elif usr_input_mm=="d":   # delete exam in json database
             print(usr_input_mm)
         elif usr_input_mm=="q":   # quit py-study-planner program
@@ -101,4 +157,3 @@ def main():
 
 if __name__=="__main__":
     main()
-    
