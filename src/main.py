16994732a1d790dbcfb66b78e01d669486f819fe
KladeRe <klaus.rehnberg@gmail.com>
2023-06-22 17:07:00 +0300

    Removed useless lines of code and added OOP

--- src/main.py
+++ src/main.py
@@ -1,46 +1,51 @@
 """code execution main menu file"""
 
 #import modules
-from scheduler import scheduler_main
-from exam import exam_main
+from scheduler import ScheduleManager
+from exam import ExamManager
 import os
 import json
-def print_user_options():
-    """presents options for user in menu"""
-    print("('e' and hit enter to enter the exam editing menu)")
-    print("('s' and hit enter to enter the schedule editing menu)")
-    print("('q' and hit enter to quit py-study-planner)")
 
-def mm_main():
-    """run main function while constant "run" is True, allows user to navigate main menu or quit"""
-    print("Welcome to py-study-planner.")
-    print("What would you like to do? Here are your options:")
-    print_user_options()
-    run=True
-    
-    if not os.path.exists('exam_data.json'): #If JSON database doesn't exist then add the database
-        open('exam_data.json',"w", encoding='utf-8')
+class MainProgram:
+    def __init__(self):
+        self.examMgr = ExamManager()
+        self.scheduleMgr = ScheduleManager()
 
-    if (os.path.getsize('exam_data.json') == 0): #If JSON database is empty then add '{}'
-        with open('exam_data.json',"w", encoding='utf-8') as exam_data_file:
-            json.dump({}, exam_data_file)
+    def print_user_options(self):
+        """presents options for user in menu"""
+        print("('e' and hit enter to enter the exam editing menu)")
+        print("('s' and hit enter to enter the schedule editing menu)")
+        print("('q' and hit enter to quit py-study-planner)")
 
-    while run is True:
-        usr_input_mm = input("")
-        if usr_input_mm=="e":     # generate schedule for exam
-            exam_main()
-            print("Welcome back from exam editing! What next?")
-            print_user_options()
-        elif usr_input_mm=="s":     # display schedules for exams
-            scheduler_main()
-            print("Welcome back from schedule editing! What next?")
-            print_user_options()
-        elif usr_input_mm=="q":   # quit py-study-planner program
-            run=False
-        else:                       # exception handling: repeat user options
-            print("input invalid. your options are:")
-            print_user_options()
-    return run
+    def mm_main(self):
+        """run main function while constant "run" is True, allows user to navigate main menu or quit"""
+        print("Welcome to py-study-planner.")
+        print("What would you like to do? Here are your options:")
+        run=True
+        
+        if not os.path.exists('exam_data.json'): #If JSON database doesn't exist then add the database
+            open('exam_data.json',"w", encoding='utf-8')
+
+        if (os.path.getsize('exam_data.json') == 0): #If JSON database is empty then add '{}'
+            with open('exam_data.json',"w", encoding='utf-8') as exam_data_file:
+                json.dump({}, exam_data_file)
+
+        while run is True:
+            self.print_user_options()
+            usr_input_mm = input("")
+            if usr_input_mm=="e":     # generate schedule for exam
+                self.examMgr.exam_main()
+                print("Welcome back from exam editing! What next?")
+            elif usr_input_mm=="s":     # display schedules for exams
+                self.scheduleMgr.scheduler_main()
+                print("Welcome back from schedule editing! What next?")
+            elif usr_input_mm=="q":   # quit py-study-planner program
+                run=False
+            else:                       # exception handling: repeat user options
+                print("input invalid. your options are:")
+                
+        return run
 
 if __name__=="__main__":
-    mm_main()
+    mainloop = MainProgram()
+    mainloop.mm_main()
--- src/scheduler.py
+++ src/scheduler.py
@@ -9,85 +9,87 @@ from operator import itemgetter
 import pandas as pd
 import numpy as np
 
-def print_user_options():
-    """print user options for the scheduling suite in menu"""
-    print("('s' and hit enter to display your schedule)")
-    print("('a' and hit enter to display your day's agenda)")
-    print("('q' and hit enter to quit the scheduling suite)")
+class ScheduleManager:
+    def __init__(self, db_file='exam_data.json'):
+        self.db_file = db_file
 
-def gen_tasks(exam_name):
-    """generate a list of tuples with task names and associated times"""
-    with open('exam_data.json',"r", encoding='utf-8') as exam_data_file:
-        loaded = json.load(exam_data_file)
-    resource_data = loaded[exam_name]['resource_data']
-    today = datetime.datetime.today().date()
-    date_data = loaded[exam_name]['exam_date']
-    date_data = datetime.datetime.strptime(date_data, '%m/%d/%Y').date()
-    t_remaining = (date_data-today).days
-    resource_names = [tup[0] for tup in resource_data]
-    resource_times = [int(tup[1])/t_remaining for tup in resource_data]
-    tasks = list(zip(resource_names,resource_times))
-    return tasks, t_remaining
+    def print_user_options(self):
+        """print user options for the scheduling suite in menu"""
+        print("('s' and hit enter to display your schedule)")
+        print("('a' and hit enter to display your day's agenda)")
+        print("('q' and hit enter to quit the scheduling suite)")
 
-def view_schedule():
-    """generate a study schedule for a given exam"""    
-    exam_name = input("What exam would you like to generate a schedule for?\n")
-    tasks = gen_tasks(exam_name)
-    schedule = {}
-    td = datetime.datetime.today()
-    tdmin = datetime.timedelta(minutes=(td.minute))
-    def convert_to_timeform(time):
-        """convert to preferred %m/%d/%Y %H:%M time format"""
-        return datetime.datetime.strftime((time),'%m/%d/%Y %H:%M')
-    for i in range(((24*tasks[1])-int(td.hour))-1):
-        raw_time = td-tdmin+datetime.timedelta(hours=i+1)
-        schedule[convert_to_timeform(raw_time)] = [np.nan]
-    def find_time(n):
-        """find first instance of window of empty values in dict"""
-        free_in_a_row = 0
-        for index, date in enumerate(schedule):     
-            if (np.isnan(schedule[date])):
-                free_in_a_row += 1
-            else:
-                free_in_a_row = 0
-            if (free_in_a_row == n):
-                return index-n+1
-            
-        return np.nan
-    schedule = pd.DataFrame(schedule).T
-    print(schedule)
-    return True
+    def gen_tasks(self, exam_name):
+        """generate a list of tuples with task names and associated times"""
+        with open('exam_data.json',"r", encoding='utf-8') as exam_data_file:
+            loaded = json.load(exam_data_file)
+        resource_data = loaded[exam_name]['resource_data']
+        today = datetime.datetime.today().date()
+        date_data = loaded[exam_name]['exam_date']
+        date_data = datetime.datetime.strptime(date_data, '%m/%d/%Y').date()
+        t_remaining = (date_data-today).days
+        resource_names = [tup[0] for tup in resource_data]
+        resource_times = [int(tup[1])/t_remaining for tup in resource_data]
+        tasks = list(zip(resource_names,resource_times))
+        return tasks, t_remaining
 
-def view_agenda():
-    """view an agenda for a given day"""
-    ...
+    def view_schedule(self):
+        """generate a study schedule for a given exam"""    
+        exam_name = input("What exam would you like to generate a schedule for?\n")
+        tasks = self.gen_tasks(exam_name)
+        schedule = {}
+        td = datetime.datetime.today()
+        tdmin = datetime.timedelta(minutes=(td.minute))
+        def convert_to_timeform(time):
+            """convert to preferred %m/%d/%Y %H:%M time format"""
+            return datetime.datetime.strftime((time),'%m/%d/%Y %H:%M')
+        for i in range(((24*tasks[1])-int(td.hour))-1):
+            raw_time = td-tdmin+datetime.timedelta(hours=i+1)
+            schedule[convert_to_timeform(raw_time)] = [np.nan]
+        def find_time(n):
+            """find first instance of window of empty values in dict"""
+            free_in_a_row = 0
+            for index, date in enumerate(schedule):     
+                if (np.isnan(schedule[date])):
+                    free_in_a_row += 1
+                else:
+                    free_in_a_row = 0
+                if (free_in_a_row == n):
+                    return index-n+1
+                
+            return np.nan
+        schedule = pd.DataFrame(schedule).T
+        print(schedule)
+        return True
 
-def del_schedule():
-    """view an agenda for a given day"""
-    ...
+    def view_agenda(self):
+        """view an agenda for a given day"""
+        ...
 
-def scheduler_main():
-    """run main function while constant "run" is True, allows user to navigate main menu or quit"""
-    print("Welcome to the scheduling suite.")
-    print("What would you like to do? Here are your options:")
-    print_user_options()
-    run=True
-    while run is True:
-        usr_input_mm = input("")
-        if usr_input_mm=="s":     # display schedules for exams
-            view_schedule()
-            print("schedule viewed! What next?")
-            print_user_options()
-        elif usr_input_mm=="a":     # view day agenda
-            view_agenda()
-            print("agenda viewed! What next?")
-            print_user_options()
-        elif usr_input_mm=="q":   # quit py-study-planner program
-            run=False
-        else:                       # exception handling: repeat user options
-            print("input invalid. your options are:")
-            print_user_options()
-    return run
+    def del_schedule(self):
+        """view an agenda for a given day"""
+        ...
+
+    def scheduler_main(self):
+        """run main function while constant "run" is True, allows user to navigate main menu or quit"""
+        print("Welcome to the scheduling suite.")
+        print("What would you like to do? Here are your options:")
+        run=True
+        while run is True:
+            self.print_user_options()
+            usr_input_mm = input("")
+            if usr_input_mm=="s":     # display schedules for exams
+                self.view_schedule()
+                print("schedule viewed! What next?")
+            elif usr_input_mm=="a":     # view day agenda
+                self.view_agenda()
+                print("agenda viewed! What next?")
+            elif usr_input_mm=="q":   # quit py-study-planner program
+                run=False
+            else:                       # exception handling: repeat user options
+                print("input invalid. your options are:")
+        return run
 
 if __name__=="__main__":
-    scheduler_main()
+    scheduleMgr = ScheduleManager()
+    scheduleMgr.scheduler_main()
